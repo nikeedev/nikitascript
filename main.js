7113549384170a0b4a47c65a17d3b7f5d28c5692
nikeedev <myftr12394@gmail.com>
2022-03-14 21:57:36 +0100

    Updates!!!!!

---
+++ main.js
@@ -0,0 +1,3 @@
+var version = "1.0-alpha";
+var build = "2022.03-1";
+console.log("NikitaScript // v" + version + " - Build " + build);
---
+++ main.ts
@@ -0,0 +1,4 @@
+var version : string = "1.0-alpha";
+var build : string = "2022.03-1";
+
+console.log("NikitaScript // v"+version+" - Build "+ build)
